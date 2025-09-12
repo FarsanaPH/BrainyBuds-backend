04e7cd12b0a907c9adfadaa17d37483f10db9276
Farsana PH <farsanaph2001@gmail.com>
2025-09-12 15:05:11 +0530

    backend updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const jsonServer = require("json-server");
 
 // 2. create server
-const Jobserver = jsonServer.create(); // Returns an Express server.
+const Schoolserver = jsonServer.create(); // Returns an Express server.
 
 // middle is a function which have ability to break the request response cycle.
 // 5. middleware to parse json format
@@ -13,13 +13,13 @@ const middleware = jsonServer.defaults();
 const router = jsonServer.router("db.json");
 
 // 6. use middleware
-Jobserver.use(middleware);
-Jobserver.use(router);
+Schoolserver.use(middleware);
+Schoolserver.use(router);
 
 // 3. set port number for the server
 const PORT = 4000 || process.env.PORT;
 
 // 4. listen to the request from the frontend to resolve the request.
-Jobserver.listen(PORT, () => {
-  console.log(`Job Server Running Successfully at port number ${PORT}`);
+Schoolserver.listen(PORT, () => {
+  console.log(`School Server Running Successfully at port number ${PORT}`);
 });
